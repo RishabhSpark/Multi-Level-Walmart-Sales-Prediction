144819a5be313af3042b300e68e48b72cae24c3e
RishabhSpark <rishabh1000khandelwal@gmail.com>
2023-09-11 01:55:30 +0800

    changes

---
+++ main.py
@@ -0,0 +1,28 @@
+# Takes user input for input values - want grid search or not
+# Model name
+# Parameters from user - range(p,d,q,P,D,Q), list(smoothing_level, trend, seasonality)
+# Evaluates the model and returns best predictions in a .csv file (path input from user and save there~, save file name to - ask from user)
+# Calls generate_predictions function from generate_predictions.py file
+
+# 124 Weeks - 
+# 250 Weeks - 
+
+
+# Pipeline which can be called by the company every time they want to update their data and generate predictions
+
+# min_p = +ve int, 2
+# max_p = +ve int, 10
+
+# p_value = range(min_p, max_p)
+
+# generate_predictions.generate_predictions(dataset, "sarima", splits=(0.75,0.125), perform_grid_search=True, p_range = p_value)
+
+# return csv file containing predictions
+
+
+
+
+
+
+# Readme.md - How to run, thora description 
+# requirements.txt
